b44b103a251a4205cc5e4b57bf63aed95cc8cd41
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-07-23 18:50:42 -0500

    [Add] Content Movie Page

---
+++ src/components/ContentMovie.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Data from "../data/data.json";
+
+import "../styles/contentmovie.scss";
+
+const Content = () => {
+  const Movies = Data.entries.filter((data) => data.programType === "movie");
+
+  return (
+    <section className="box-movie">
+      {Movies.map((data) => (
+        <div className="box-card-movie">
+          <img src={data?.images["Poster Art"]?.url} alt={data.title} />
+
+          <p>{data.title}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Content;
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Movies = () => {
-  return <div>Movies</div>;
+import Navbar from "../components/Navbar";
+import ContentMovie from "../components/ContentMovie";
+
+const Movies = ({ tag }) => {
+  return (
+    <>
+      <Navbar tag={tag} />
+      <ContentMovie />
+    </>
+  );
 };
 
 export default Movies;
