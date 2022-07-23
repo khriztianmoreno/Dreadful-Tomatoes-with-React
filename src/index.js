8af25af688381ac2b88c1095acecae59dce8d263
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-07-23 18:48:56 -0500

    [Add] Add Routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/Landingpage";
+import Movies from "./pages/Movies";
+import TvShows from "./pages/Tvshows";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/movies" element={<Movies tag="movies" />} />
+        <Route path="/tvshows" element={<TvShows tag="tvshows" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
