aee4f4c012d9dd8dddcc855fa781e8c0f5d4d717
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-07-24 13:43:39 -0500

    [Add] Add tvShow component

--- src/pages/Tvshows.js
+++ src/pages/Tvshows.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Navbar from "../components/Navbar";
+import Content from "../components/Content";
+import Pagination from "../components/Pagination";
+import Footer from "../components/Footer";
+
+import Data from "../data/data.json";
 
 const Tvshows = ({ tag }) => {
+  const Tvshows = Data.entries.filter((data) => data.programType === "series");
+
+  const [tvshows] = useState(Tvshows);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [moviesPerPage] = useState(10);
+
+  const indexOfLastPost = currentPage * moviesPerPage;
+  const indexOfFirstPost = indexOfLastPost - moviesPerPage;
+  const currentMovies = tvshows.slice(indexOfFirstPost, indexOfLastPost);
+  const numberOfPages = Math.ceil(tvshows.length / moviesPerPage);
+
   return (
     <>
       <Navbar tag={tag} />
+      <Content movies={currentMovies} />
+      <Pagination
+        numberOfPages={numberOfPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <Footer />
     </>
   );
 };
