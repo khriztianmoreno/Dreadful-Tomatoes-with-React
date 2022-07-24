fc3620a543d507d882db42cde4b54364c334cf24
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-07-24 12:55:41 -0500

    [Add] Add pagination component and styles

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "../components/Navbar";
 import ContentMovie from "../components/ContentMovie";
+import Pagination from "../components/Pagination";
+import Footer from "../components/Footer";
+
+import Data from "../data/data.json";
 
 const Movies = ({ tag }) => {
+  const Movies = Data.entries.filter((data) => data.programType === "movie");
+
+  const [movies] = useState(Movies);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [moviesPerPage] = useState(10);
+
+  const indexOfLastPost = currentPage * moviesPerPage;
+  const indexOfFirstPost = indexOfLastPost - moviesPerPage;
+  const currentMovies = movies.slice(indexOfFirstPost, indexOfLastPost);
+  const numberOfPages = Math.ceil(movies.length / moviesPerPage);
+
   return (
     <>
       <Navbar tag={tag} />
-      <ContentMovie />
+      <ContentMovie movies={currentMovies} />
+      <Pagination
+        numberOfPages={numberOfPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <Footer />
     </>
   );
 };
